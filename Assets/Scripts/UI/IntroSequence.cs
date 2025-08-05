1d87d0c683b4ef0e520431806ea270b9095fb8bc
Shivansh Chaudhary <97225407+parapara29@users.noreply.github.com>
2025-08-05 22:26:56 +0530

    feat: add character portraits to intro sequence

--- Assets/Scripts/UI/IntroSequence.cs
+++ Assets/Scripts/UI/IntroSequence.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public struct PanelData
 {
     public Sprite background;
+    public Sprite character;
     public string text;
     public float holdTime;
 }
@@ -21,12 +23,15 @@ public class IntroSequence : MonoBehaviour
 
     GameObject currentPanel;
     GameObject panelPrefab;
+    Image backgroundImage;
+    Image characterImage;
+    TMP_Text dialogueText;
 
     void Start()
     {
         if (hasSeenIntro)
         {
-            gameObject.SetActive(false);
+            SceneManager.LoadScene("DemoScene");
             return;
         }
 
@@ -78,17 +83,25 @@ public class IntroSequence : MonoBehaviour
         var cg = currentPanel.GetComponentInChildren<CanvasGroup>();
         if (cg != null) cg.alpha = 0f;
 
-        var data = panels[currentIndex];
-        var img = currentPanel.GetComponentInChildren<Image>();
-        if (img != null) img.sprite = data.background;
-
-        var txt = currentPanel.GetComponentInChildren<TMP_Text>();
-        if (txt != null) txt.text = data.text;
+        backgroundImage = currentPanel.transform.Find("Background")?.GetComponent<Image>();
+        characterImage = currentPanel.transform.Find("Character")?.GetComponent<Image>();
+        dialogueText = currentPanel.transform.Find("DialogueText")?.GetComponent<TMP_Text>();
 
         var skip = currentPanel.GetComponentInChildren<Button>();
         if (skip != null) skip.onClick.AddListener(Skip);
 
+        var data = panels[currentIndex];
+        if (backgroundImage != null) backgroundImage.sprite = data.background;
+        if (characterImage != null) characterImage.sprite = data.character;
+        if (dialogueText != null)
+        {
+            dialogueText.text = data.text;
+            dialogueText.maxVisibleCharacters = 0;
+        }
+
         yield return FadeIn(currentPanel);
+        if (dialogueText != null)
+            yield return RevealText(dialogueText);
         yield return new WaitForSeconds(data.holdTime);
     }
 
@@ -122,12 +135,22 @@ public class IntroSequence : MonoBehaviour
         ShowNextPanel();
     }
 
+    IEnumerator RevealText(TMP_Text txt)
+    {
+        txt.ForceMeshUpdate();
+        int total = txt.textInfo.characterCount;
+        for (int i = 0; i <= total; i++)
+        {
+            txt.maxVisibleCharacters = i;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
     void EndIntro()
     {
         PlayerPrefs.SetInt("IntroSeen", 1);
         PlayerPrefs.Save();
         hasSeenIntro = true;
-        SendMessage("StartGuidance", SendMessageOptions.DontRequireReceiver);
-        gameObject.SetActive(false);
+        SceneManager.LoadScene("DemoScene");
     }
 }
